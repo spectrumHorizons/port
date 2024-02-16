1c2d8ca1c9e5225f2e450eadae1a50a9ef38fb6e
spectrumHorizons <160190516+spectrumHorizons@users.noreply.github.com>
2024-02-16 07:26:45 +0000

    .

--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./use-click-outside";
-export * from "./useLockPage";
-export {default as useModal} from "./useModal";
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import {RouterProvider} from "react-router-dom";
-
-import './index.css'
-import {routers} from "./routes.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <RouterProvider router={routers}/>
-);
